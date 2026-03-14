2acbfc7d2e7a9520d8be3eee1a4af98a7d8d5c92
MrPal28 <arindampal669@gmail.com>
2026-03-14 16:23:39 +0530

    feat: Add interview service and enable notification service to the master Docker Compose configuration.

--- interview-service-ai/AudioAnswerConverterService/main.py
+++
@@ -1,8 +0,0 @@
-"""Main module to start the Audio Answer Converter Service by initializing the Kafka consumer with the event handler."""
-# Import Headers
-from Components.EventHandler import eventHandler
-from Components.KafkaConsumer import startConsumer
-
-# # Entry point of Audio To Answer Converter Service
-if __name__ == "__main__": 
-    startConsumer(eventHandler)
--- interview-service-ai/InterviewReportService/main.py
+++
@@ -1,5 +0,0 @@
-from Components.RedisCache import handleMessage
-from Components.KafkaConsumer import startConsumer
-
-if __name__ == "__main__":
-     startConsumer(handleMessage)
